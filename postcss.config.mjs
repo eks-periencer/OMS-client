453c92653c203f6831ae1d06fb13d988b32c392b
Xoli <xolinxiweni@gmail.com>
2025-09-08 15:35:24 +0200

    repo structure migration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
-export default config
+export default {
+  plugins: [tailwindcss, autoprefixer],
+}
